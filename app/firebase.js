a93d14a247809ee6f6404f7facf718fa1bfdfb08
Dafaild25 <154031943+Dafaild25@users.noreply.github.com>
2024-05-27 10:25:28 -0500

    cambiando nombre

--- app/firebase.js
+++ app/firebase.js
@@ -1,7 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
 // TODO: Add SDKs for Firebase products that you want to use
+
 // https://firebase.google.com/docs/web/setup#available-libraries
+//para autenticar
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-auth.js"
 //enlace para bdd
 import { getFirestore, collection, addDoc, getDocs, onSnapshot, deleteDoc, doc, getDoc, updateDoc } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-firestore.js";
